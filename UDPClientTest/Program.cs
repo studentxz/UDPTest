3c97698fd74afa43cd50944bd3f006a1fe7ee4d9
studentxz <308795606@qq.com>
2019-09-29 15:26:05 +0800

    first commit of the udptest.

---
+++ UDPClientTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //建立UDPClientSocket,参数2：udp协议以数据报的方式传输，参数3：UDP协议
+            Socket udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress ip = IPAddress.Parse(GetLocalAddress(5));
+
+            EndPoint ipAddress = new IPEndPoint(ip, 43999);
+
+            Console.WriteLine("请输入需要发送给服务端的信息，以回车结束。");
+            //输入客户端需要发送给服务端的信息
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                Console.WriteLine(message);
+                udpClient.SendTo(data, ipAddress);
+            }
+            
+        }
+
+        /// <summary>
+        /// 获取本机IP地址    3：本机外网IPV4;  5：本机局域网IPV4
+        /// </summary>
+        /// <param name="addressNumber"></param>
+        /// <returns></returns>
+        private static string GetLocalAddress(int addressNumber)
+        {
+            IPAddress[] addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+
+            if (addressList.Length < 1)
+            {
+                return "";
+            }
+            return addressList[addressNumber].ToString();
+        }
+    }
+}
---
+++ UDPTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //建立UDPSocket 参数2：udp协议以数据报的方式传输，参数3：UDP协议
+            Socket udpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            
+            //为udp服务器绑定ip
+            IPAddress ip = IPAddress.Parse(getLocalIpAddress(5));
+            EndPoint ipAddress = new IPEndPoint(ip, 43999);
+            udpServer.Bind(ipAddress);
+
+            //接收数据 本机的所有IP地址，所有可用的端口
+            EndPoint clientAddress = new IPEndPoint(IPAddress.Any, 0);
+            string message;
+            byte[] data = new byte[1024];
+            int length = 0;
+
+            //把数据的来源放到第二个参数上
+            while (true)
+            {
+                length = udpServer.ReceiveFrom(data, ref clientAddress);
+                message = Encoding.UTF8.GetString(data, 0, length);
+                Console.WriteLine("从IP:" + (clientAddress as IPEndPoint).Address + "取到了消息:" + message);
+            }
+        }
+        /// <summary>
+        /// 获取本机IP地址，3：外网IPV4地址，5：局域网IPV4地址
+        /// </summary>
+        /// <param name="addressNumber"></param>
+        /// <returns></returns>
+        private static string getLocalIpAddress(int addressNumber)
+        {
+            //获得本机局域网IP
+            IPAddress[] addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+
+            if (addressList.Length < 1)
+            {
+                return "";
+            }
+
+            //5是外网IPV4
+            //6是以太网IPV4
+            return addressList[addressNumber].ToString();
+        }
+    }
+}
